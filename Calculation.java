dd9ef3b81dafda44f2516c238da3672e1fb4a405
Adit-17 <yadavaditya17196@gmail.com>
2023-04-01 21:40:48 +0530

    done

---
+++ Calculation.java
@@ -0,0 +1,15 @@
+package day2;
+// wap to print sum of two number
+public class Calculation {
+
+	
+	public static void main(String[] args) {
+     int a=10;
+     int b=20;
+     int c=a+b;
+     System.out.println("the sum is ="  +  "c");
+     
+
+	}
+
+}
